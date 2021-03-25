1efcafa9dcea8e47d0e266f00fd8186a8ddd0520
Kassim <kassimhuggins@macbook-pro.attlocal.net>
2021-03-24 21:54:21 -0700

    3/4-2/6 resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,71 @@
-// const resolvers = {
-//     Query: {
-//       helloWorld: () => {
-//         return 'Hello world!';
-//       }
-//     }
-//   };
+
+const { AuthenticationError } = require('apollo-server-express');
+
+
+
+const { signToken } = require('../utils/auth');
+
+const { user, User } = require ('../models');
+
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+       if (context.user) {
+         const userData = await User.findOne ({ _id: context.user._id })
+         .select('-_v -password')
+         return userData;
+
+       }
+       
+       throw new AuthenticationError('Youre not logged in dude!');
+
+      }
+    },
+
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+
+        return { token, user};
+      },
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne ({ email });
+        if(!user) {
+          throw new AuthenticationError('your credintials are not correct!');
+
+        }
+        const correctPw = await user.isCorrectPassword(password);
+if (!correctPw) {
+  throw new AuthenticationError('your credintials are not correct');
+
+}
+
+const token = signToken (user);
+return { token, user};
+      },
+    saveBook: async (parent, { input }, { user}) => {
+      if (user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: user._id},
+          { $addToSet: { savedBooks: input }},
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError('Must be logged in');
+    },
+    removeBook: async (parent, { bookId }, {user}) => {
+      if (user) {
+        const updatedUser = await User.findOneAndUpdate (
+          { _id: user._id},
+          { $pull: { savedBooks: { bookId: bookId}}},
+          { new: true, runValidators: true}
+        );
+        throw new AuthenticationError('Must be logged in');
+      }
+    }
+    }
+  };
   
-  // module.exports = resolvers;
+  module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+
+
 const path = require('path');
+
+
+
+const routes = require('./routes')
 const db = require('./config/connection');
-const routes = require('./routes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
